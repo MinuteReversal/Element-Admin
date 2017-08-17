0b2ce3d1584de978c99e51355011be42890b2420
114233763@qq.com <mailzy@vip.qq.com>
2017-08-17 16:19:32 +0800

    fontawesome is awesome

---
+++ scripts/vue-compnent/el-progress-button/JavaScript.js
@@ -0,0 +1,37 @@
+﻿/**
+ * author      : 反转的分针
+ * date        : 20170810
+ * mail        : 114233763@qq.com
+ * description : 区域选择
+ * @param {String} placeholder
+ * @param {String} returnType v-model
+ * @returns {String|Array} item.DataValue
+ */
+var elProgressButton = {
+    template:
+    '<div>' +
+    '<button class="el-button el-button-default" @click="$emit(\'click\')">' +
+    '<span v-show="percentage === 0">下载</span>' +
+    '<el-progress type="line" v-show="percentage > 0" :percentage="percentage" :width=30 :stroke-width=2 ></el-progress>' +
+    '</button>' +
+    '</div>'
+    ,
+    props: {
+        "percentage": {
+            "type": Number,
+            "default": 0
+        }
+    },
+    data: function () {
+        return {
+        };
+    },
+    methods: {
+
+    },
+    components: {
+        "el-progress": ELEMENT.Progress
+    },
+    mounted: function () {
+    }
+};
